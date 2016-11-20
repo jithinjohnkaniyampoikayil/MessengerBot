d86c16a83d22defaca0151b88d2433312518913f
Jithin John <jithin.john537@gmail.com>
2016-11-20 12:49:29 +0530

    Update MessengerBot.php

--- MessengerBot.php
+++ MessengerBot.php
@@ -5,18 +5,8 @@
 	// parameters
 	$hubVerifyToken = '*****';
 	$accessToken = "*******";
-	$salutation=array("hai","da","hello","daaa","kooi","hi","bro");
-	$aftersalutation=array("watsup","dude","man","so");
-	$positiveaftersalutation=array("yes","yup","athe","ss");
-	$negativeaftersalutation=array("no","nope","noway","sorry","alla");
-	$questions_h=array("how are you?","how do you do?","enthoke ondu?");
-	$questions_w=array("what are you?","what do you do?");
-	$glims=array("hmm","ok","mm","k");
-	$symbols=array("lol","cool","nice");
-	$badwords_m=array("poda","patty","thendi");
-	$badwords_e=array("fuck","shit","mother");
-	$thanks=array("well done","super","thank you","welcome","awesome");
-
+	$salutation=array("hai","hello","hi","bro");
+	
 	// check token at setup
 	if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 		echo $_REQUEST['hub_challenge'];
@@ -35,56 +25,7 @@
 	    })) {
 		$answer = "Hi";
 	}
-	else if(array_filter($aftersalutation, function($el) use ($messagetext) {
-		return ( strpos($el, $messagetext) !== false );
-	})){
-		$answer = 'Going well, ;)';
-	}
-	else if(array_filter($questions_w, function($el) use ($messagetext) {
-		return ( strpos($el, $messagetext) !== false );
-	})){
-		$answer = 'I am a Bot';
-	}
-	else if(array_filter($questions_h, function($el) use ($messagetext) {
-		return ( strpos($el, $messagetext) !== false );
-	})){
-		$answer = 'I am a fine';
-	}
-	else if(array_filter($questions, function($el) use ($messagetext) {
-        return ( strpos($el, $messagetext) !== false );
-    })){
-  		$answer = 'I am in kochi';
-  	}
-  	else if(array_filter($glims, function($el) use ($messagetext) {
-  		return ( strpos($el, $messagetext) !== false );
-  	})){
-  		$answer = 'Hmmm.. :)';
-  	}
-  	else if(array_filter($symbols, function($el) use ($messagetext) {
-  		return ( strpos($el, $messagetext) !== false );
-  	})){
-  		$answer = 'Nice..';
-  		
-  	}
-  	else if(array_filter($badwords_m, function($el) use ($messagetext) {
-  		return ( strpos($el, $messagetext) !== false );
-  	})){
-  		$answer = 'Hey dont use bad words..';
-  	}
-  	else if(array_filter($badwords_e, function($el) use ($messagetext) {
-  		return ( strpos($el, $messagetext) !== false );
-  	})){
-  		$answer = 'Hey dont use bad words..';
-  	}
-  	else if(array_filter($thanks, function($el) use ($messagetext) {
-  		return ( strpos($el, $messagetext) !== false );
-  	})){
-  		$answer = 'Thanks.. ;)';
-  	}
-  	else{
-  		//$answer = 'Bot under construction';
-  	}
-
+	
 	$response = [
 	'recipient' => [ 'id' => $senderId ],
 	'message' => [ 'text' => $answer ]
